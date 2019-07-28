93e5a1e01b5dc1a538cbeb26878db7d5c6eb07bc
Mobarok Hossen <mobarokaiub@gmail.com>
2019-07-28 13:51:55 +0600

    Update table.sql

--- table.sql
+++ table.sql
@@ -3,6 +3,7 @@ CREATE TABLE `categories` (
   `category_image` varchar(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `display_name` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
   `parent_id` int(10) UNSIGNED DEFAULT NULL,
   `status` int(1) DEFAULT 1,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
